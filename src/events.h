bc98994b6cb844bf63d2e6d44b92b38629a4a30a
Andrea Giardina <andrea@etiqa.it>
2021-06-06 12:00:15 +0200

    Custom events centralization

---
+++ src/events.h
@@ -0,0 +1,14 @@
+//
+//  events.h
+//  GardenPhotos
+//
+//  Created by Andrea Giardina on 06/06/21.
+//
+
+#ifndef events_h
+#define events_h
+
+#define GP_EVT_IMPORT_FOLDER     (wxID_HIGHEST + 1)
+#define GP_EVT_TOGGLE_FAVORITE   (wxID_HIGHEST + 2)
+
+#endif /* events_h */
